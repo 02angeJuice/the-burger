32e5d01bdcefcf98a57ac2c655856cee2e3f6a43
banndit <feles.catt@gmail.com>
2020-04-13 14:05:13 +0700

    Initial Setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles.css';
+
+const App = () => {
+  return (
+    <div>
+      <p className={styles.title}>App</p>
+    </div>
+  );
+};
+
+export default App;
